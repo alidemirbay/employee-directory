e8e0ba61cb3e2d172e9957fbaf1f09d44248aa78
alidemirbay <alidemirbay@gmail.com>
2021-03-01 08:40:50 -0800

    adds header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid bg-primary">
+        <div className="container">
+          <h1 className="text-center text-light">Employee Directory</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
